2765db9b1cf640de7e0a73e75e5bb578d7e20717
Andy Tran <153461965+Andyt212@users.noreply.github.com>
2025-02-09 18:27:48 -0600

    Update server.py

--- server.py
+++ server.py
@@ -244,7 +244,3 @@ if __name__ == "__main__":
     # Bind to 0.0.0.0 and use the environment variable for the port
     port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
     app.run(host='0.0.0.0', port=port)
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
